9a6ba8f42f35ca64759a7755c2d54386d68ed54c
atkmehmet <durmazmehmet47@gmail.com>
2026-01-29 00:52:35 +0300

    add result class

---
+++ CustomerApi/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomerApiNew.Application
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public string? Message { get; }
+        public T? Data { get; }
+
+        private Result(bool isSuccess, string? message, T? data)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+            Data = data;
+        }
+
+
+
+        public static Result<T> Success (T data,string? messages = null)
+            => new Result<T> (true,messages,data);
+
+        public static Result<T> Failure( string messages)
+            => new Result<T> (false, messages,default);
+    }
+}
